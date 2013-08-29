f0d953608efc888f412c30b04e86e1dd5845009a
Faith-Anne Kocadag <faith.anne@gmail.com>
2013-08-28 20:46:57 -0400

    Creating a new project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acm::Application.config.secret_token = 'bf660811751564c27274b291d354183773c5ef25ccd2b802f952af6faeea844e743b60e54ebe7b6aeb370a8ff757c244ac98c03f2aeba510d055b954dfe67d2a'
