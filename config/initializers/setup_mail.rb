4304312b0f2e009467d4ce67131075a31b677279
Faith-Anne Kocadag <faith.anne@gmail.com>
2013-09-12 12:08:44 -0400

    Fixing mail initializer.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "acm.armstrong@gmail.com",
-  :password             => ENV['GMAIL_PASSWORD']
+  :password             => ENV['GMAIL_PASSWORD'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
